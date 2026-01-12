53021c55954cec7ee0311925a91ae149d6d97b70
Daniel Brotsky <dan@brotsky.com>
2026-01-11 20:41:40 -0800

    Move tauri app and python module to their own repos.

--- tauri/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import HomeScreen from '$lib/components/HomeScreen.svelte';
-</script>
-
-<div class="flex min-h-screen w-full items-center justify-center bg-gray-200 p-3">
-	<div class="h-[95vh] w-full overflow-auto rounded-xl bg-white">
-		<HomeScreen />
-	</div>
-</div>
