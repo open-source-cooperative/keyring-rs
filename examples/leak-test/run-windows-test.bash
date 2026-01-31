0d50f344080ebd7bf00a69bc47035cd180942566
Daniel Brotsky <dan@brotsky.com>
2026-01-30 16:20:14 -0800

    Improve the leak tests.
    1. Make sure the leaked string is in modified memory, so that it shows as leaked on Mac.
    2. Fail the test if the core dump command fails.
    3. Always wait for the leak-test program to finish.
    4. Exit with code 0 on success, 1 on failure.

--- examples/leak-test/run-windows-test.bash
+++ examples/leak-test/run-windows-test.bash
@@ -8,11 +8,14 @@
 # sample store (which does leak) to make sure this test is working.
 #
 # By default, this "shows the sausage being made" as the test runs.
-# If you don't want to see that, pipe the out of this script into
+# If you don't want to see that, pipe the output of this script into
 # a grep for the word `TEST`, as in:
 #     bash run-windows-test.bash | grep TEST
 # Then you will just see the test program output (on stderr)
 # together with the results of the testing.
+#
+# You can also just ignore the output completely and look at the
+# exit code: if it's 0, the test passed, if it's 1 it didn't.
 #
 # You must have installed procdump64a and strings64a from
 # the Microsoft SysInternals suite before you run this.
@@ -22,15 +25,26 @@ echo Running...
 # shellcheck disable=SC2086
 DELAY_SECS=5 cargo run --example leak-test -- ${1:-windows} &
 sleep 2
-echo Dumping...
 rm -f /tmp/keyring-test.dmp
+echo Dumping...
 /c/Windows/procdump64a.exe -ma -o leak-test /tmp/leak-test.dmp
+# shellcheck disable=SC2181
+if [ $? != 0 ]; then
+  echo TEST ABORT - procdump failure
+  echo Waiting for leak test to clean up...
+  wait %1
+  exit 1
+fi
 echo Grepping...
 /c/Windows/strings64a.exe /tmp/leak-test.dmp | grep -H -n super-duper-password
 # shellcheck disable=SC2181
 if [ $? == 0 ]; then
+  exit_code=1
   echo TEST FAILED
 else
+  exit_code=0
   echo TEST SUCCEEDED.
 fi
+echo Waiting for leak test to clean up...
 wait %1
+exit $exit_code
